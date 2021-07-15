f8c9914d81632422dcb1adfb7ce36c258a1b5bb3
ohouens <ryan@ohouens.com>
2021-07-15 13:58:15 +0200

    bedrooms/rooms/size errors fixed

--- data/data_validation.py
+++ data/data_validation.py
@@ -12,14 +12,15 @@ cursor = db.inventory.find({})
 
 #init columns
 city = []
+zipcode = []
 description = []
-doors = []
 image = []
 link = []
 price = []
 rooms = []
+bedrooms = []
 size = []
-type = []
+estate_type = []
 zone = []
 
 
@@ -27,26 +28,57 @@ for inventory in cursor:
     # pprint(inventory)
     city.append(inventory['city'])
     description.append(inventory['description'])
-    doors.append(inventory['doors'])
     image.append(inventory['image'])
     link.append(inventory['link'])
-    price.append(inventory['rooms'])
-    size.append(inventory['size'])
-    type.append(inventory['type'])
+    price.append(inventory['price'])
+    estate_type.append(inventory['type'])
     zone.append(inventory['zone'])
+
+    b = None
+    r = None
+    s = None
+
+    if(len(inventory['doors'].split()) > 1):
+        if(inventory['doors'].split()[1] == "p"):
+            r = inventory['doors'].split()[0]
+        if(inventory['doors'].split()[1] == "ch"):
+            b = inventory['doors'].split()[0]
+        if(inventory['doors'].split()[1] == "m²"):
+            s = inventory['doors'].split()[0]
+
+    if(len(inventory['rooms'].split()) > 1):
+        if(inventory['rooms'].split()[1] == "p"):
+            r = inventory['rooms'].split()[0]
+        if(inventory['rooms'].split()[1] == "ch"):
+            b = inventory['rooms'].split()[0]
+        if(inventory['rooms'].split()[1] == "m²"):
+            s = inventory['rooms'].split()[0]
+
+    if(len(inventory['size'].split()) > 1):
+        if(inventory['size'].split()[1] == "p"):
+            r = inventory['size'].split()[0]
+        if(inventory['size'].split()[1] == "ch"):
+            b = inventory['size'].split()[0]
+        if(inventory['size'].split()[1] == "m²"):
+            s = inventory['size'].split()[0]
+
+    bedrooms.append(b)
+    rooms.append(r)
+    size.append(s)
 
 d = {
         'city': city,
         'description': description,
-        'doors': doors,
         'image': image,
         'link': link,
         'price': price,
+        'bedrooms': bedrooms,
+        'rooms': rooms,
         'size': size,
-        'type': type,
+        'type': estate_type,
         'zone': zone
     }
 data_to_csv = pd.DataFrame(data=d)
 data_to_csv.to_csv("grand_paris_estates.csv")
 
-print(data_to_csv.head())
+print(data_to_csv[["bedrooms", "rooms", "size"]].head())
