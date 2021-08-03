91fbd2b50e6ff9f6c7c2906423b2e583c0dcf15b
ohouens <ryan@ohouens.com>
2021-08-03 10:01:55 +0200

    init of generic views and templates' style

--- PinEstate/adverts/urls.py
+++ PinEstate/adverts/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'adverts'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name="detail"),
+    path('<int:estate_id>/pin/', views.pin, name="pin"),
 ]
--- PinEstate/adverts/views.py
+++ PinEstate/adverts/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.views import generic
 
-def index(request):
-    return HttpResponse("Here will be the list of ads.")
+from .models import Estate
+
+class IndexView(generic.ListView):
+    template_name = "adverts/index.html"
+    context_object_name = "latest_estate_list"
+
+    """Return the last 5 estates"""
+    def get_queryset(self):
+        return Estate.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Estate
+    template_name = "adverts/detail.html"
+
+def pin(request, estate_id):
+    return HttpResponse("To pin estate %s" % estate_id)
